92fa0f5ad8def9bd959bcdefd8f75bb9d3fae224
rostodev <rostodev@gmail.com>
2023-12-30 11:56:14 +0100

    CoursJS-2023

---
+++ script.js
@@ -0,0 +1,70 @@
+
+// ******************VARIABLES
+   //-------Declarations de variables ---(var, let, const )
+    //   age = 23; // ou var age = 23;
+    //   let firstName2 = 'infinity';
+    //   const fruitc ="banane";
+
+
+//--Primitives/Types valeurs
+       //(String, number, undefined, null, Boolean, Symbol)
+
+   //***numbers */
+
+   let firstName = 'infinity'; //String
+   let last_name = "eva"; //String
+   
+   let age = 23; // number
+   let name =undefined;  //undefined
+   let fullName = null; //null
+   let isApproved = true; //Boolean
+   let symbole = Symbol("monSymbole"); // Une donnée unique et non modifiable
+   
+//    console.log("je me norme " + last_name +" et j'ai " +age+ " ans"); 
+   
+   console.log(`je me norme  ${last_name}  et j'ai  ${age} ans`);
+  
+
+
+   const fruit ="banane";
+    age = 23;
+    // age=24;
+   
+
+//--------Types references-----------
+     //(Objects, Tableau, Fonctions)
+
+     //-------- Objects : est une structure de données qui permet de stocker des ensembles de clés/valeurs et des entités plus complexes
+
+
+     let mane1 = "eva";
+     let age1 = 12
+
+     let person = {
+        name1 : "eva",
+        age1 : 12,
+     };
+        //acceder a une propriété de l'objet
+    //  console.log(person.name1);
+    //  console.log(person.age1);
+
+
+     //ou acceder a une proprieté de l'objet
+
+     person['name1'] = "Myra";
+    //  console.log( person['name1'] = "Myra");
+
+    // Tableau 
+
+
+    let fruits = ["Banane", "Pomme", "carotte"]
+
+    fruits[3]="Avocat"
+    console.log(fruits[3]);
+    
+
+
+     
+
+
+
